c30e67e0c8a9e0d11785aea1eef127abdc8b6d8d
Josh <joshmoore2@gmail.com>
2014-01-24 15:04:17 -0400

    Sample Search Form
    Sample search form to make api calls

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel='stylesheet' type='text/css' href='css/style.css' />
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400' rel='stylesheet' type='text/css'>
+	<title>Instagrub</title>
+</head>
+<body>
+<div id="wrap">
+	<div id="header">Instagrub</div>
+
+	<div id="search_box">
+		<form name="post_msg" action="get.php" method="post">
+			Search: <input type="text" name="query" maxlength="30" /><br />
+			<input type="submit" value="submit" />
+		</form>
+	</div>
+</div>
+</body>
+</html>
