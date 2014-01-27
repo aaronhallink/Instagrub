008c2688b84120faaee31ed554de07482628b616
joelnav <joel.navarrete27@gmail.com>
2014-01-27 18:30:58 -0400

    Styles and Script

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,13 @@ var amount = new Array();
 var i;
 
 function displayIngredient(){
-/*if (e.keyCode == 13) {*/
 	var name = document.getElementById('ingredients').value;
 	var quantity = document.getElementById('amount').value;
-	//for(i = 0; i<ingredients_list.length; i++){
-	//}
+
 	if (quantity == "")
 		quantity = "1";
 	if(checkIngredient(name)==1){
+		console.log("HERE");
 		alert("You already entered that!");
 		return;
 	}
@@ -23,9 +22,9 @@ function displayIngredient(){
 		console.log(ingredients[0]);
 		for(i = 0; i<ingredients.length; i++){
 			if(i!=ingredients.length-1)
-				output += (amount[i] + ":" + ingredients[i] + "\n");
+				output += (amount[i] + " :" + ingredients[i] + "\n");
 			else
-				output += (amount[i] + ":" + ingredients[i]  "\n");
+				output += (amount[i] + " :" + ingredients[i] + "\n");
 		}
 		document.getElementById('input_target').innerHTML = output;
 		document.getElementByid('input_target').placeholder=null;
@@ -54,15 +53,4 @@ function revertImage(){
 	var target = document.getElementById('addImage');
 	target.src = "images/add_unpressed.png";
 }
-	/*function outputName(){
-	var name = document.getElementById('name').value;
-	if(name!="")
-		document.getElementById('name_target').innerHTML = "<p> Hello " + name + "</p>";
-	else{
-		alert("Please enter your name!");
-		document.getElementById('name_target').innerHTML = null;
-	}
-}*/
-	/*return false;*/
-// }
 
