e3e48c732a6fc943918657bf6c5f6fa7900459d7
joelnav <joel.navarrete27@gmail.com>
2014-01-27 17:41:41 -0400

    Front End
    Basic Front End stuff

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+//var ingredients_list = newArray();
+var output="" ;
+
+
+
+function displayIngredient(){
+/*if (e.keyCode == 13) {*/
+	var name = document.getElementById('ingredients').value;
+	//for(i = 0; i<ingredients_list.length; i++){
+	//}
+	if(name!=""){
+		output+= (name + "\n");
+		document.getElementById('input_target').innerHTML = output;
+		document.getElementByid('input_target').placeholder=null;
+	}
+	else{
+		alert("Enter an ingredient!");
+		document.getElementById('input_target').innerHTML = null;
+	}
+	console.log(output);
+
+}
+function changeImage(){
+	console.log("TEST");
+	var target = document.getElementById('addImage');
+	target.src = "images/add_pressed.png";
+}
+
+function revertImage(){
+	var target = document.getElementById('addImage');
+	target.src = "images/add_unpressed.png";
+}
+	/*function outputName(){
+	var name = document.getElementById('name').value;
+	if(name!="")
+		document.getElementById('name_target').innerHTML = "<p> Hello " + name + "</p>";
+	else{
+		alert("Please enter your name!");
+		document.getElementById('name_target').innerHTML = null;
+	}
+}*/
+	/*return false;*/
+// }
+
