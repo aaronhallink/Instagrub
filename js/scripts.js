de10d35a37f908149b5ddf3996178068c3f34e13
joelnav <joel.navarrete27@gmail.com>
2014-01-27 18:20:31 -0400

    scripts change

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
 //var ingredients_list = newArray();
 var output="" ;
 var ingredients = new Array();
+var amount = new Array();
 var i;
 
 function displayIngredient(){
 /*if (e.keyCode == 13) {*/
 	var name = document.getElementById('ingredients').value;
+	var quantity = document.getElementById('amount').value;
 	//for(i = 0; i<ingredients_list.length; i++){
 	//}
+	if (quantity == "")
+		quantity = "1";
+	if(checkIngredient(name)==1){
+		alert("You already entered that!");
+		return;
+	}
 	if(name!=""){
 		ingredients.push(name);
+		amount.push(quantity);
 		output = "";
 		console.log(ingredients[0]);
 		for(i = 0; i<ingredients.length; i++){
 			if(i!=ingredients.length-1)
-				output += (ingredients[i] + ", ");
+				output += (amount[i] + ":" + ingredients[i] + "\n");
 			else
-				output += ingredients[i]
+				output += (amount[i] + ":" + ingredients[i]  "\n");
 		}
 		document.getElementById('input_target').innerHTML = output;
 		document.getElementByid('input_target').placeholder=null;
@@ -25,8 +34,15 @@ function displayIngredient(){
 		alert("Enter an ingredient!");
 		document.getElementById('input_target').innerHTML = null;
 	}
-	console.log(output);
+}
 
+function checkIngredient(name){
+	for(i=0; i<ingredients.length; i++){
+		if(ingredients[i]==name){
+			return 1;
+		}
+	}
+	return 0;
 }
 function changeImage(){
 	console.log("TEST");
