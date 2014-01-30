342ca5c3726bbd98b43c2b34f7161c98a0e5baa5
MiladAwkar <milad.awkar24@gmail.com>
2014-01-30 17:07:59 -0400

    Documentation
    documented script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,23 @@ var output="" ;
 var ingredients = new Array();
 var amount = new Array();
 var i;
-
+//once an ingredient and quantity is entered in the text field, it gets displayed
 function displayIngredient(){
 	var name = document.getElementById('ingredients').value;
 	var quantity = document.getElementById('amount').value;
-
+	//if no or negative quantity is inputed, it sets to 1 as default
 	if (quantity == "")
 		quantity = "1";
+	else if (quantity < 0)
+		quantity = "1";
+		
+	//if the ingredient is already in the ingredients array
 	if(checkIngredient(name)==1){
 		console.log("HERE");
 		alert("You already entered that!");
 		return;
 	}
+	//ingredient gets added
 	if(name!=""){
 		ingredients.push(name);
 		amount.push(quantity);
@@ -29,12 +34,13 @@ function displayIngredient(){
 		document.getElementById('input_target').innerHTML = output;
 		document.getElementByid('input_target').placeholder=null;
 	}
+	//if nothing gets added
 	else{
 		alert("Enter an ingredient!");
 		document.getElementById('input_target').innerHTML = null;
 	}
 }
-
+//checks if the ingredient is already in the array
 function checkIngredient(name){
 	for(i=0; i<ingredients.length; i++){
 		if(ingredients[i]==name){
@@ -43,6 +49,7 @@ function checkIngredient(name){
 	}
 	return 0;
 }
+//these two change the image whether the image is pressed or released
 function changeImage(){
 	console.log("TEST");
 	var target = document.getElementById('addImage');
