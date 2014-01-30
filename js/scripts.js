c91c94094c2956815f8a5d91cfb9bdf09bb98676
joelnav <joel.navarrete27@gmail.com>
2014-01-30 17:43:50 -0400

    Removed changes for merge

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,28 @@
 var output="" ;
 var ingredients = new Array();
 var i;
-
+//once an ingredient and quantity is entered in the text field, it gets displayed
 function displayIngredient(){
+<<<<<<< HEAD
 	console.log("TEST");
 	var name = document.getElementById('query').value;
 	console.log(name);
+=======
+	var name = document.getElementById('ingredients').value;
+	var quantity = document.getElementById('amount').value;
+	//if no or negative quantity is inputed, it sets to 1 as default
+	if (quantity == "")
+		quantity = "1";
+	else if (quantity < 0)
+		quantity = "1";
+		
+	//if the ingredient is already in the ingredients array
+>>>>>>> 4d3835661712bd4f75f4ea2157408721c2ab059c
 	if(checkIngredient(name)==1){
 		alert("You already entered that!");
 		return;
 	}
+	//ingredient gets added
 	if(name!=""){
 		ingredients.push(name);
 		output = "";
@@ -26,12 +39,13 @@ function displayIngredient(){
 		document.getElementById('input_target').innerHTML = output;
 		document.getElementById('input_target').placeholder=null;
 	}
+	//if nothing gets added
 	else{
 		alert("Enter an ingredient!");
 		document.getElementById('input_target').innerHTML = null;
 	}
 }
-
+//checks if the ingredient is already in the array
 function checkIngredient(name){
 	for(i=0; i<ingredients.length; i++){
 		if(ingredients[i]==name){
@@ -40,7 +54,13 @@ function checkIngredient(name){
 	}
 	return 0;
 }
+<<<<<<< HEAD
 function changeImageAdd(){
+=======
+//these two change the image whether the image is pressed or released
+function changeImage(){
+	console.log("TEST");
+>>>>>>> 4d3835661712bd4f75f4ea2157408721c2ab059c
 	var target = document.getElementById('addImage');
 	target.src = "images/add_pressed.png";
 }
