15514e897dc138791a3816df96a92c56bef1d3ba
Josh <joshmoore2@gmail.com>
2014-01-31 17:18:58 -0400

    Added user login system
    Basic system for logging in and out. No error handling is done yet.

--- index.php
+++ index.php
@@ -5,25 +5,38 @@
 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400' rel='stylesheet' type='text/css'>
 	<title>Instagrub</title>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="js/scripts.js"></script>
-
 </head>
 <body>
+<?php
+	session_start();
+	require('config.php');
+?>
 <div id="wrap">
-		<div id="login">
+
+		<?php
+		if (isset($_SESSION['valid_user']))
+		{
+			echo "Hello, ". $_SESSION['valid_user'];
+			echo "<br/><a href='logout.php'>Log out</a>";
+
+		}
+		else
+		{
+			?>
+
+			<div id="login">
 			<a href="login.php">Login</a> or <a href="register.php">Sign up</a>
-		</div>
-		<div id="header"></div>
+			</div>
+
+			<?php
+		}
+		?>
+	<div id="header">Instagrub</div>
 
 	<div id="search_box">
 		<form name="post_msg" action="get.php" method="post">
-			<input type="text" id="query" name="query" maxlength="150" placeholder="Enter ingredient here"/>
-			<img id="addImage" src="images/add_unpressed.png" onclick="displayIngredient()" onmousedown="changeImageAdd()" onmouseup="revertImageAdd()" height="35px" align="middle"/><br />
-			<textarea readonly rows="30" cols="50" id="input_target" alignment="center" placeholder="This will display what has already been inputted above" style="background-color:#ffeabd">
-			</textarea>
-			<button type="submit"><img src="images/search_unpressed.png" id="searchImage" 
-			onmousedown="changeSearchImage()" onmouseup="revertSearchImage()" width="200px"/></button>
+			Search: <input type="text" name="query" maxlength="30" /><br />
+			<input type="submit" value="submit" />
 		</form>
 	</div>
 </div>
