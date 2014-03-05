b1514f9d70cd0f025cd26c858aac1b51d8151cc7
MiladAwkar <milad.awkar24@gmail.com>
2014-03-05 17:12:45 -0400

    Making the email_address unique
    Making the email_address unique will prevent duplicates

--- add_new_user.php
+++ add_new_user.php
@@ -8,12 +8,17 @@
 	
 		*/	
 			//check if email is already in the database
-			function doubleEmail($email)
+			/*function doubleEmail($email)
 			{
 				$conn = mysql_connect('localhost:3306', 'root', '' );
 				mysql_select_db('jamjam', $conn);
-				
-			}
+
+				$query = "SELECT email_address FROM user WHERE email_address = '$email';";
+				$result = mysql_query($query);
+				$row = mysql_fetch_row($result);
+
+
+			}*/
 			function checkEmail($email_f) 
 			{
 				//Query Database and check if the username is taken
