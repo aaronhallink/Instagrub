a72bbbb6cf6bc8753035cce8ddddddaec41515d3
Josh <joshmoore2@gmail.com>
2014-03-20 16:59:15 -0300

    Revert "Mobile Search page"
    This reverts commit 1a1479da91dff946bd80b8e65ea8e93f0bf86efe.

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=320">
-
 <link rel='stylesheet' type='text/css' href='css/style.css' />
 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400' rel='stylesheet' type='text/css'>
@@ -21,36 +19,36 @@
 	session_start();
 	require('config.php');
 ?>
+<div id="wrap">
 
-	<div id="header">
-		<div style="float:left;margin-top:-6px;color:white;">
-			<h1>Instagrub</h1>
-		</div>
 		<?php
 		if (isset($_SESSION['valid_user']))
 		{
 			echo "Hello, ".$_SESSION['uname'];
 			echo "<br/><a href='logout.php'>Log out</a>  - <a href='ingredients.php'>Ingredients</a>";
+
 		}
 		else
 		{
 			?>
 
-			<div id="login" style="float:right;">
+			<div id="login">
 			<a href="login.php">Login</a> or <a href="register.php">Sign up</a>
 			</div>
 
 			<?php
 		}
 		?>
-	</div>
-<div id="wrap">
+
+	<div id="header"></div>
 	<!--This is where the buttons and area where the ingredients are listed-->
 	<div id="search_box">
 		<form name="post_msg" method="post">
-			<center><div id="search_and_btn"><input type="text" id="query" name="query" maxlength="150" placeholder="Enter ingredient here"/><span  class="small_btn"  onclick="displayIngredient()"/>Add</span></div></center>
-			<center><textarea readonly rows="30" cols="50" id="input_target" alignment="center" placeholder="This will display what has already been inputted above" ></textarea></center>
-			<center><a href="" id="searchImage" onclick="recipeSearch()" />Find Recipes</a></center>
+			<input type="text" id="query" name="query" maxlength="150" placeholder="Enter ingredient here"/>
+			<img id="addImage" src="images/add_unpressed.png"  onclick="displayIngredient()" onmousedown="changeImageAdd()" onmouseup="revertImageAdd()" height="35px" align="middle"/><br />
+			<textarea readonly rows="30" cols="50" id="input_target" alignment="center" placeholder="This will display what has already been inputted above" style="background-color:#ffeabd">
+			</textarea>
+			<img src="images/search_unpressed.png" id="searchImage" onclick="recipeSearch()" onmousedown="changeImageSearch()"  onmouseup="revertImageSearch()"  width="200px"/>
 	</div>
 	<!--This is where the results will be displayed-->
 	<div id="results_target">
