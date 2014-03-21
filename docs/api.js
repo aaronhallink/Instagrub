c66a8c1c7ad90f349a2a13452bc3d298721c7b50
asrz <zinckalexander@gmail.com>
2014-03-20 23:57:03 -0300

    fixed master
    Re-updated docs and testing. I hate Git.

--- docs/api.js
+++ docs/api.js
@@ -1,9 +1,19 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "api"
+        "api",
+        "register",
+        "scripts"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "scripts"
+    ],
+    "allModules": [
+        {
+            "displayName": "scripts",
+            "name": "scripts",
+            "description": "A collection of general functions used in the Intsagrub application"
+        }
+    ]
 } };
 });
