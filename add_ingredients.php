35f86e8de29a970dcb3d7a330b2cc17fe17c0631
MiladAwkar <xmiladx24@hotmail.com>
2014-03-24 21:49:21 -0300

    Update add_ingredients.php

--- add_ingredients.php
+++ add_ingredients.php
@@ -3,15 +3,19 @@
 
 	$ingredient_name = htmlspecialchars($_POST['ingredient_name']);
 
-	$conn = mysql_connect('localhost:3306', 'root', '' );
-
-	mysql_select_db('jamjam', $conn);
+	require_once('config.php');
+	
+	$conn=mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+	if (mysqli_connect_errno())
+	{
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 
 	//insert ingredient and user_id into database			
 
 	$query = "INSERT INTO ingredients(ingredient_name, user_id) VALUES ('$ingredient_name', '".$_SESSION['user_id']."');";
 
-	mysql_query($query);
-	mysql_close();
+	mysqli_query($conn, $query);
+	mysqli_close($conn);
 	header('Location: ingredients.php');
 ?>
