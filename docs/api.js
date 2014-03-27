dd8226e85421c99968f4221501d2badf10de754a
asrz <zinckalexander@gmail.com>
2014-03-27 19:01:18 -0300

    Fixed docs
    Docs folder wasn't in project. Whoops.

---
+++ docs/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "api",
+        "register",
+        "scripts"
+    ],
+    "modules": [
+        "scripts"
+    ],
+    "allModules": [
+        {
+            "displayName": "scripts",
+            "name": "scripts",
+            "description": "A collection of general functions used in the Intsagrub application"
+        }
+    ]
+} };
+});
